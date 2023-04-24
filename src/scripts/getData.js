3988689fef68e90eefe4fccff50ca7ca31f17c6f
MaxS43 <85175053+MaxS43@users.noreply.github.com>
2023-04-24 17:06:02 +0200

    Added Functionality of Parsing Data from DB into whole generation, consumption and netto

--- src/scripts/getData.js
+++ src/scripts/getData.js
@@ -1,71 +1,49 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-function getDataFromAPI() {
-    return __awaiter(this, void 0, void 0, function* () {
-        try {
-            const response = yield fetch('https://ems-syp.000webhostapp.com/api/');
-            //console.log(response);
-            const data = yield response.json();
-            console.log(data);
-            let dataArray = Array.isArray(data) ? data : [data];
-            //console.log(dataArray);
-            for (let i = 0; i < dataArray.length; i++) {
-                const convertedDate = new Date(convertDateTime(dataArray[i].timestamp));
-                if (!isNaN(convertedDate.getTime())) {
-                    dataArray[i].time = convertedDate;
-                }
-                else {
-                    dataArray[i].time = new Date(0);
-                }
-            }
-            dataArray = processJSON(dataArray);
-            //DEV AUSGABE
-            let genArray = [];
-            let conArray = [];
-            for (let i = 0; i < dataArray.length; i++) {
-                //console.log(dataArray[i]);
-                if (dataArray[i].type === 'generation') {
-                    genArray.push(dataArray[i]);
-                }
-                else if (dataArray[i].type === 'consumption') {
-                    conArray.push(dataArray[i]);
-                }
+async function getDataFromAPI() {
+    try {
+        const response = await fetch('https://ems-syp.000webhostapp.com/api/');
+        const data = await response.json();
+        console.log(data);
+        let dataArray = Array.isArray(data) ? data : [data];
+        for (let i = 0; i < dataArray.length; i++) {
+            const convertedDate = new Date(convertDateTime(dataArray[i].timestamp));
+            if (!isNaN(convertedDate.getTime())) {
+                dataArray[i].time = convertedDate;
+            } else {
+                dataArray[i].time = new Date(0);
             }
         }
-        catch (error) {
-            console.error('Fehler beim Abrufen der Daten:', error);
-        }
-    });
+        dataArray = processJSON(dataArray);
+        return dataArray;
+    } catch (error) {
+        console.error('Fehler beim Abrufen der Daten:', error);
+    }
 }
+
 function processJSON(jsonArray) {
     const sortedArray = jsonArray.sort((a, b) => {
         const aDate = new Date(a.time);
         const bDate = new Date(b.time);
         return aDate.getTime() - bDate.getTime();
     });
+
     let generation = 0;
     let consumption = 0;
+
     const resultArray = sortedArray.map((data) => {
         if (data.type === 'generation') {
             generation += data.value;
-        }
-        else if (data.type === 'consumption') {
+        } else if (data.type === 'consumption') {
             consumption += data.value;
         }
         return data;
     });
+
     const net = generation - consumption;
+
     return resultArray.concat({ net });
 }
+
 function convertDateTime(inputDateTime) {
-    //console.log(inputDateTime);
     const dateParts = inputDateTime.split(" ")[0].split("-");
     const timeParts = inputDateTime.split(" ")[1].split(":");
     const day = dateParts[2];
@@ -77,10 +55,32 @@ function convertDateTime(inputDateTime) {
     const convertedDateTime = `${day}.${month}.${year} ${hours}:${minutes}:${seconds}`;
     return convertedDateTime;
 }
-function main() {
-    return __awaiter(this, void 0, void 0, function* () {
-        yield getDataFromAPI(); // Aufruf der Funktion
-    });
+
+function calculateNetUsage(data) {
+    let generated = 0;
+    let consumed = 0;
+
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].type === "generation") {
+            generated += parseInt(data[i].measurement);
+        } else if (data[i].type === "consumption") {
+            consumed += parseInt(data[i].measurement);
+        }
+    }
+
+    const net = generated - consumed;
+
+    return {
+        generated,
+        consumed,
+        net
+    };
 }
+
+async function main() {
+    const data = await getDataFromAPI(); // Aufruf der Funktion
+    const dataCalc = calculateNetUsage(data);
+    console.log(dataCalc);
+}
+
 main();
-//# sourceMappingURL=getData.js.map
