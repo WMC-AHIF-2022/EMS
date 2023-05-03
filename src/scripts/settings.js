08562907887571849c09ca1f4ee3f0453d77e525
MaxS43 <85175053+MaxS43@users.noreply.github.com>
2023-05-03 20:15:17 +0200

    Settingspage and Backend

--- src/scripts/settings.js
+++ src/scripts/settings.js
@@ -5,3 +5,29 @@ async function getSettings(){
   console.log("SettingsJSON: ", data);
   return data;
 }
+
+async function sendSettings() {
+  const unitOfElectricity = document.getElementById("unitOfElectricity").value;
+  const currency = document.getElementById("currency").value;
+  const defaultStats = document.getElementById("defaultStats").value;
+  const homeCity = document.getElementById("homeCity").value;
+
+  const url = `https://ems-syp.000webhostapp.com/api/setSettings.php?unitOfElectricity=${unitOfElectricity}&currency=${currency}&defaultStats=${defaultStats}&homeCity=${homeCity}`;
+
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      console.log("Settings successfully saved!");
+	  document.getElementById("infoBox").innerHTML = "Einstellungen erfolgreich gespeichert!";
+	  document.getElementById("infoBox").style.backgroundColor = "#90EE90 ";
+    } else {
+      console.log("Error: ", response.status);
+	  document.getElementById("infoBox").innerHTML = "Fehler beim Speichern der Einstellungen!";
+	  document.getElementById("infoBox").style.backgroundColor = "red";
+    }
+  } catch (error) {
+    console.log("Error: ", error.message);
+	document.getElementById("infoBox").innerHTML = "Fehler beim Speichern der Einstellungen!";
+	document.getElementById("infoBox").style.backgroundColor = "red";
+  }
+}
