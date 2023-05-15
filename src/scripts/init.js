d750c155396db728ae2a2df59b429901a9777c6e
MaxS43 <85175053+MaxS43@users.noreply.github.com>
2023-05-15 12:52:05 +0200

    Fixed Bug in Loadingspinner

--- src/scripts/init.js
+++ src/scripts/init.js
@@ -3,6 +3,7 @@ async function init(){
     getWeatherData(settings[3].value);
     let demodata = [0,0,0,0,0,0,0,0,0,0];
     drawChart(demodata, demodata);
+    document.getElementById("loadingChart").style.display = "none";
     // drawChart()
     // let data = getDataFromAPI();
     // Date date = new Date();
